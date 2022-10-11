527312a5984319fd166bf8716b0b744e75eda4d4
Kirill Andronow <andronov.km@phystech.edu>
2022-10-11 22:23:47 +0300

    add code

---
+++ 5-1.py
@@ -0,0 +1,39 @@
+from pickle import TRUE
+from signal import signal
+import RPi.GPIO as GPIO
+import time
+
+def binary(a):
+    return[int(i) for i in bin(a)[2:].zfill(8)]
+
+GPIO.setmode(GPIO.BCM)
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+level = 256
+comp = 4
+troyka = 17
+GPIO.setup(dac, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(comp, GPIO.IN)
+
+def numDAC(value):
+    signal = binary(value)
+    GPIO.output(dac, signal)
+    return signal
+
+
+
+try:
+    while True:
+        for value in range(level):
+            signaL = numDAC(value)
+            V = value/level * 3.3
+            compValue = GPIO.input(comp)
+            if compValue == 0:
+                print("ADC value = {:^3} -> {}, Voltage = {:.2f}".format(value, signaL, V))
+
+except KeyboardInterrupt:
+    print('\nПрограмма была остановлена с клавиатуры')
+
+finally:
+    GPIO.output(dac, GPIO.LOW)
+    GPIO.cleanup()
